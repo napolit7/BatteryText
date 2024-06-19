262fd8785e2b41727ce14b2b5566b1f38d798d78
napolit7 <michael.o.napolitano@gmail.com>
2024-06-19 15:17:52 -0400

    Respring functionality

--- batterytextpreferences/BTRootListController.m
+++ batterytextpreferences/BTRootListController.m
@@ -1,15 +1,25 @@
 #import <Cephei/HBPreferences.h>
 #import <Preferences/PSSpecifier.h>
+#import <spawn.h>
+#import <sys/wait.h>
 #import "BTRootListController.h"
 
 @implementation BTRootListController
 
-- (NSArray *)specifiers {
-	if (!_specifiers) {
-		_specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
+	- (NSArray *)specifiers {
+		if (!_specifiers) {
+			_specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
+		}
+
+		return _specifiers;
 	}
 
-	return _specifiers;
-}
+	- (void)respringDevice {
+    	pid_t pid;
+		int status;
+		const char *argv[] = {"/usr/bin/sbreload", NULL};
+		posix_spawn(&pid, argv[0], NULL, NULL, (char *const *)argv, NULL);
+		waitpid(pid, &status, WEXITED);
+	}
 
 @end
